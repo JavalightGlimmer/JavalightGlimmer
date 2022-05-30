4a7e5ccb23958f4eb924ef5b64099db3c85b6976
Javalight Glimmer <980624557@qq.com>
2022-05-30 14:24:41 +0800

    changed in 2022.5.29 14:24

---
+++ src/main/java/com/mmall/common/ServerResponse.java
@@ -0,0 +1,65 @@
+package com.mmall.common;
+
+import org.codehaus.jackson.annotate.JsonIgnore;
+import org.codehaus.jackson.map.annotate.JsonSerialize;
+
+import java.io.Serializable;
+
+@JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)//不要null，这样error的时候不展示data
+public class ServerResponse<T> implements Serializable {
+    private int status;
+    private String msg;
+    private T data;
+
+    private ServerResponse(int status){
+        this.status = status;
+    }
+    private ServerResponse(int status, T data){
+        this.status = status;
+        this.data = data;
+    }
+    private ServerResponse(int status,String msg,T data){
+        this.status = status;
+        this.msg = msg;
+        this.data = data;
+    }
+    private ServerResponse(int status,String msg){
+        this.status = status;
+        this.msg = msg;
+    }
+    @JsonIgnore//序列化后不会显示在json里面
+    public boolean isSuccess(){
+        return this.status == ResponseCode.SUCCESS.getCode();
+    }
+    public int getStatus(){
+        return status;
+    }
+    public T getData(){
+        return data;
+    }
+    public String getMsg(){
+        return msg;
+    }
+//  <T>也是为了定义一种范型，用来提前声明形参的类型是一个范型变量。
+    public static <T> ServerResponse<T> createBySuccess() {
+        return new ServerResponse<T>(ResponseCode.SUCCESS.getCode());
+    }
+    public static <T> ServerResponse<T> createBySuccessMessage(String msg){
+        return new ServerResponse<T>(ResponseCode.SUCCESS.getCode(),msg);
+    }
+    public static <T> ServerResponse<T> createBySuccess(T data){
+        return new ServerResponse<T>(ResponseCode.SUCCESS.getCode(),data);
+    }
+    public static <T> ServerResponse<T> createBySuccess(String msg, T data){
+        return new ServerResponse<T>(ResponseCode.SUCCESS.getCode(),msg,data);
+    }
+    public static <T> ServerResponse<T> createByError(){
+        return new ServerResponse<T>(ResponseCode.ERROR.getCode(),ResponseCode.ERROR.getDesc());
+    }
+    public static <T> ServerResponse<T> createByErrorMessage(String errorMsg){
+        return new ServerResponse<T>(ResponseCode.ERROR.getCode(),errorMsg);
+    }
+    public static <T> ServerResponse<T> createByErrorCodeMessage(int errorCode, String errorMsg){
+        return new ServerResponse<T>(errorCode,errorMsg);
+    }
+}
